c5dd57213eba39842d4692feb77faa27f1ca5a28
Gustavo Arazin <gustarazin@gmail.com>
2026-03-21 17:05:09 -0300

    Update ClaudeCyclesARZN.lean

--- ClaudeCyclesARZN_Lean_M0/ClaudeCyclesARZN.lean
+++ ClaudeCyclesARZN_Lean_M0/ClaudeCyclesARZN.lean
@@ -1,13 +1,14 @@
-import ClaudeCyclesARZN.Core
-import ClaudeCyclesARZN.Torus
-import ClaudeCyclesARZN.Fiber
-import ClaudeCyclesARZN.EvenRule
-import ClaudeCyclesARZN.EvenRuleFacts
-import ClaudeCyclesARZN.EvenResidualSupport
-import ClaudeCyclesARZN.ReturnMap
-import ClaudeCyclesARZN.Reduction
-import ClaudeCyclesARZN.Orbit
-import ClaudeCyclesARZN.ReturnIterates
-import ClaudeCyclesARZN.FiberSlices
-import ClaudeCyclesARZN.GlobalLift
-import ClaudeCyclesARZN.StrongReduction
+ import ClaudeCyclesARZN.Core
+ import ClaudeCyclesARZN.Torus
+ import ClaudeCyclesARZN.Fiber
+ import ClaudeCyclesARZN.EvenRule
+ import ClaudeCyclesARZN.EvenRuleFacts
+ import ClaudeCyclesARZN.EvenResidualSupport
++import ClaudeCyclesARZN.EvenResidualNormalForm
+ import ClaudeCyclesARZN.ReturnMap
+ import ClaudeCyclesARZN.Reduction
+ import ClaudeCyclesARZN.Orbit
+ import ClaudeCyclesARZN.ReturnIterates
+ import ClaudeCyclesARZN.FiberSlices
+ import ClaudeCyclesARZN.GlobalLift
+ import ClaudeCyclesARZN.StrongReduction
